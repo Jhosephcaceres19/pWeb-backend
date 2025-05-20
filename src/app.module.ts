a17eab4a5901414b3f372b9c91706f7b64919949
JHOSEPH <jhoseph@ZEROCOOL.ZEROCOOL>
2025-05-20 17:31:25 -0400

    add modele:student, teacher and auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TeacherModule } from './teacher/teacher.module';
+import { StudentModule } from './student/student.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [TeacherModule, StudentModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
